54725bf8af5368231327e00cc3509b20232775b4
Colin Lansbergen <colinlansbergen@MacBook-Pro-van-Colin.local>
2020-07-27 12:06:34 +0200

    NEXT_PUBLIC_ added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-const contentful = require("contentful");
 import { useEffect, useState } from "react";
 import Head from "next/head";
 import Job from "../components/Job"
 import Navbar from "../components/Navbar";
+import * as contentful from "contentful";
 
-const client = require("contentful").createClient({
-  space: NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  accessToken: NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+const client = contentful.createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFULSPACE,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFULAPI
 });
 
 function jobPage() {
